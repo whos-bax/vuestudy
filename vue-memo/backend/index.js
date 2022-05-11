bd0edae7dff31ea633375a3ec355c455c3a9c36c
who <loot04@korea.ac.kr>
2022-05-11 11:09:02 +0900

    axios

---
+++ vue-memo/backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const memos = ["메모 1 내용, 메모 2 내용, 메모 3 내용"];
+
+app.get("/memos", (req, res) => {
+  res.send(memos);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
