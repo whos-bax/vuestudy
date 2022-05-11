fdfbba64738444561cb2df5ba34f1eb7c451f706
who <loot04@korea.ac.kr>
2022-05-11 10:55:14 +0900

    scss 해결, node 버전 업그레이드

--- vue-memo/src/components/Memo.vue
+++ vue-memo/src/components/Memo.vue
@@ -1,23 +1,46 @@
 <template>
   <div class="memo">
+    <div class="act">
+      <button class="btn btn-primary" @click="add()">+추가</button>
+    </div>
     <ul>
-      <li>메모 1</li>
-      <li>메모 2</li>
-      <li>메모 3</li>
-      <li>메모 4</li>
+      <li v-for="(d, idx) in state.data" :key="idx">{{ d }}</li>
     </ul>
   </div>
-</template>  
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  setup() {
+    const state = reactive({
+      data: [],
+    });
+    const add = () => {
+      state.data.push("추가된 메모내용");
+    };
+
+    return { state, add };
+  },
+};
+</script>
 
 <style lang="scss" scoped>
-.memo ul {
+.memo {
+  .act {
+    text-align: right;
+    padding: 10px 10px 5px;
+  }
+  ul {
     list-style: none;
-    padding: 0;
+    padding: 15px 0;
     margin: 0;
 
     li {
-        padding: 15px;
-        margin: 5px;
+      padding: 15px;
+      margin: 5px;
+      border: 1px solid #eee;
     }
+  }
 }
 </style>
